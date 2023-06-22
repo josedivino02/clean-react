81df33f78a1c4b4e7fe6bb82c16d2807e6e78bad
José Divino <josedivinooficial02@gmail.com>
2023-06-22 17:51:45 -0300

    refactor: isolate SUT creation

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,11 +2,20 @@ import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
 
 describe('RemoteAuthentication', () => {
-  it('Should call HttpClient with correct URL', async () => {
-    const url = 'any_url';
-    const httpPostClientSpy = new HttpPostClientSpy();
-    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+  let sut: RemoteAuthentication;
+  let httpPostClientSpy: HttpPostClientSpy;
+  let url: string;
 
+  beforeAll(() => {
+    url = 'any_url';
+  });
+
+  beforeEach(() => {
+    httpPostClientSpy = new HttpPostClientSpy();
+    sut = new RemoteAuthentication(url, httpPostClientSpy);
+  });
+
+  it('Should call HttpClient with correct URL', async () => {
     await sut.auth();
 
     expect(httpPostClientSpy.url).toBe(url);
