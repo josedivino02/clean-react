d0f34ed07c9e1231d901f6ffa4fd88ed625be341
José Divino <josedivinooficial02@gmail.com>
2023-06-28 11:25:14 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Login } from '@/application/pages';
+import { Router } from '@/application/components';
 
 ReactDOM.render(
-  <Login />,
+  <Router />,
   document.getElementById('main')
 )
