4aeecb79bbf9e872c951d5748c5cd887f1e02945
José Divino <josedivinooficial02@gmail.com>
2023-06-28 17:28:46 -0300

    refactor: move SUT creation

--- src/application/pages/login/login.spec.tsx
+++ src/application/pages/login/login.spec.tsx
@@ -1,22 +1,27 @@
-import { render } from '@testing-library/react'
+import { type RenderResult, render } from '@testing-library/react'
 import React from 'react'
 import Login from './login'
 
 describe('Login Components', () => {
+  let sut: RenderResult
+
+  beforeEach(() => {
+    sut = render(<Login />)
+  })
+
   test('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+    const errorWrap = sut.getByTestId('error-wrap')
 
     expect(errorWrap.childElementCount).toBe(0)
 
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
 
-    const emailStatus = getByTestId('email-status')
+    const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
 
-    const passwordStatus = getByTestId('password-status')
+    const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
   })
