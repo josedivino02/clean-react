137e49d49a5f29fc9ea755ea6361cfeb10a1155e
José Divino <josedivinooficial02@gmail.com>
2023-06-29 11:09:14 -0300

    refactor: move SUT creation

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -3,16 +3,18 @@ import { RequiredFieldValidation } from './required-field-validation';
 import { faker } from '@faker-js/faker';
 
 describe('RequiredFieldValidation', () => {
-  it('should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email');
+  let sut: RequiredFieldValidation;
 
+  beforeEach(() => {
+    sut = new RequiredFieldValidation(faker.database.column());
+  });
+
+  it('should return error if field is empty', () => {
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError());
   });
 
   it('should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email');
-
     const error = sut.validate(faker.word.words());
     expect(error).toBeFalsy();
   });
