5ec9df5310c1fcd38cb0f699a97a80f0e5c5c647
José Divino <josedivinooficial02@gmail.com>
2023-06-29 16:22:53 -0300

    test: ensure ValidationComposite uses corrects validations

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,10 +1,10 @@
 import { Login } from '@/application/pages';
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory';
 import React from 'react';
-import { makeValidation } from './login-validation-factory';
+import { makeLoginValidation } from './login-validation-factory';
 
 export const makeLogin: React.FC = () => {
   return (
-    <Login authentication={makeRemoteAuthentication()} validation={makeValidation()} />
+    <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} />
   )
 };
--- src/main/factories/pages/login/login-validation-factory.ts
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -3,7 +3,7 @@ import {
   ValidationComposite,
 } from '@/validation/validators';
 
-export const makeValidation = (): ValidationComposite => {
+export const makeLoginValidation = (): ValidationComposite => {
   return ValidationComposite.build([
     ...ValidationBuilder.field('email').required().email().build(),
     ...ValidationBuilder.field('password').required().min(5).build(),
