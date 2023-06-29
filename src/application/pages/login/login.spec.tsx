55ac92d9751384ab94365576f0f02e808ab21f2a
José Divino <josedivinooficial02@gmail.com>
2023-06-29 00:59:34 -0300

    test: ensure Login present spinner on submit

--- src/application/pages/login/login.spec.tsx
+++ src/application/pages/login/login.spec.tsx
@@ -83,4 +83,17 @@ describe('Login Components', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(false)
   })
+
+  it('should show spinner on submit', () => {
+    validationStub.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+
+    const submitButton = sut.getByTestId('submit')
+    fireEvent.click(submitButton)
+    const spinner = sut.getByTestId('spinner')
+    expect(spinner).toBeTruthy()
+  })
 })
