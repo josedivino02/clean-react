6d1cd4459e2ddf97011df088650a695185dbde2d
José Divino <josedivinooficial02@gmail.com>
2023-06-29 16:53:56 -0300

    refactor: move SUT creation

--- src/application/components/input/input.spec.tsx
+++ src/application/components/input/input.spec.tsx
@@ -1,17 +1,21 @@
 import Context from '@/application/contexts/form/form-context';
-import { render } from '@testing-library/react';
+import { render, type RenderResult } from '@testing-library/react';
 import React from 'react';
 import Input from './input';
 
 describe('Input Component', () => {
-  it('should begin with readOnly', () => {
-    const { getByTestId } = render(
+  let sut: RenderResult
+
+  beforeEach(() => {
+    sut = render(
       <Context.Provider value={{ state: {} }}>
         <Input name='field' />
       </Context.Provider >
     )
+  })
 
-    const input = getByTestId('field') as HTMLInputElement
+  it('should begin with readOnly', () => {
+    const input = sut.getByTestId('field') as HTMLInputElement
 
     expect(input.readOnly).toBe(true)
   })
