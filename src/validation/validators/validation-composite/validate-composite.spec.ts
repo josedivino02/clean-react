c9707e8801ab83934e692b6cb30640ac3a9abd67
José Divino <josedivinooficial02@gmail.com>
2023-06-29 12:40:28 -0300

    refactor: move SUT creation

--- src/validation/validators/validation-composite/validate-composite.spec.ts
+++ src/validation/validators/validation-composite/validate-composite.spec.ts
@@ -2,16 +2,22 @@ import { ValidationComposite } from './validation-composite';
 import { FieldValidationSpy } from '../test/mock-field-validation';
 
 describe('ValidationComposite', () => {
-  it('should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field');
-    fieldValidationSpy.error = new Error('first_error_message');
-    const fieldValidationSpyTwo = new FieldValidationSpy('any_field');
-    fieldValidationSpyTwo.error = new Error('second_error_message');
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpyTwo,
-    ]);
+  let fieldValidationSpy: FieldValidationSpy;
+  let fieldValidationSpyTwo: FieldValidationSpy;
+  let sut: ValidationComposite;
 
+  beforeAll(() => {
+    fieldValidationSpy = new FieldValidationSpy('any_field');
+    fieldValidationSpyTwo = new FieldValidationSpy('any_field');
+    fieldValidationSpy.error = new Error('first_error_message');
+    fieldValidationSpyTwo.error = new Error('second_error_message');
+  });
+
+  beforeEach(() => {
+    sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpyTwo]);
+  });
+
+  it('should return error if any validation fails', () => {
     const error = sut.validate('any_field', 'any_value');
 
     expect(error).toBe('first_error_message');
