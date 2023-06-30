d003e59ce5180cee346a553788a3a2de92cafd63
José Divino <josedivinooficial02@gmail.com>
2023-06-30 16:34:04 -0300

    refactor: move SUT creation

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -3,15 +3,22 @@ import 'jest-localstorage-mock';
 import { LocalStorageAdapter } from './local-storage-adapter';
 
 describe('LocalStorageAdapter', () => {
+  let sut: LocalStorageAdapter;
+  let key: string;
+  let value: string;
+
+  beforeAll(() => {
+    key = faker.database.column();
+    value = faker.word.words();
+  });
+
   beforeEach(() => {
+    sut = new LocalStorageAdapter();
+
     localStorage.clear();
   });
 
   it('Should call localStorage with correct values', async () => {
-    const sut = new LocalStorageAdapter();
-    const key = faker.database.column();
-    const value = faker.word.words();
-
     await sut.set(key, value);
 
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
