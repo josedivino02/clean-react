b5e7752eb2f3f28e4099aab6090ca703b54b701a
José Divino <josedivinooficial02@gmail.com>
2023-07-04 22:33:23 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/sign-up-validation-factory.spec.ts
@@ -0,0 +1,23 @@
+import {
+  ValidationBuilder,
+  ValidationComposite,
+} from '@/validation/validators';
+import { makeSignUpValidation } from './sign-up-validation-factory';
+
+describe('SignUpValidationFactory', () => {
+  it('should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation();
+
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('name').required().min(4).build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation')
+          .required()
+          .min(5)
+          .build(),
+      ]),
+    );
+  });
+});
