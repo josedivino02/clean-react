21f6c2eef709b4f0032bebd12612eee5df21b021
José Divino <josedivinooficial02@gmail.com>
2023-07-09 19:55:43 -0300

    test: ensure Signup present correct error if email is in use

--- src/main/test/cypress/support/login-mocks.ts
+++ src/main/test/cypress/support/login-mocks.ts
@@ -4,6 +4,7 @@ import * as Helper from './http-mocks';
 export const mockInvalidCredentialsError = (): void => {
   Helper.mockInvalidCredentialsError(/login/);
 };
+
 export const mockUnexpectedError = (): void => {
   Helper.mockUnexpectedError(/login/, 'POST');
 };
