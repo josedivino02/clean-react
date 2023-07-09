8b634262f8d446f1bbdabb66cf39a04830dfd068
José Divino <josedivinooficial02@gmail.com>
2023-07-09 19:48:25 -0300

    test: ensure SignUp present valid state if form is valid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -36,4 +36,20 @@ describe('SignUp', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('name').focus().type(faker.person.fullName());
+    FormHelper.testInputStatus('name');
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    FormHelper.testInputStatus('email');
+
+    const password = faker.internet.password();
+
+    cy.getByTestId('password').focus().type(password);
+    FormHelper.testInputStatus('password');
+    cy.getByTestId('passwordConfirmation').focus().type(password);
+    FormHelper.testInputStatus('passwordConfirmation');
+    cy.getByTestId('submit').should('not.have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
 });
