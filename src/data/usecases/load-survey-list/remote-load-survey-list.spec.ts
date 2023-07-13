063d51273d70fb26dba72f628730c84679a9906d
José Divino <josedivinooficial02@gmail.com>
2023-07-12 22:30:55 -0300

    refactor: move SUT Creation factory

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -3,10 +3,17 @@ import { faker } from '@faker-js/faker';
 import { RemoteLoadSurveyList } from './remote-load-survey-list';
 
 describe('RemoteLoadSurveyList', () => {
+  let url: string;
+  let httpGetClientSpy: HttpGetClientSpy;
+  let sut: RemoteLoadSurveyList;
+
+  beforeEach(() => {
+    url = faker.internet.url();
+    httpGetClientSpy = new HttpGetClientSpy();
+    sut = new RemoteLoadSurveyList(url, httpGetClientSpy);
+  });
+
   it('should call HttpGetClient with correct URL', async () => {
-    const url = faker.internet.url();
-    const httpGetClientSpy = new HttpGetClientSpy();
-    const sut = new RemoteLoadSurveyList(url, httpGetClientSpy);
     await sut.loadAll();
 
     expect(httpGetClientSpy.url).toBe(url);
