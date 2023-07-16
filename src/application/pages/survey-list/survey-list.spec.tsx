02d69c1c944c0a13a91e41ce98f602c1675a59f3
José Divino <josedivinooficial02@gmail.com>
2023-07-16 16:32:31 -0300

    feat: ensure SurveyList calls LoadSurveyList Components

--- src/application/pages/survey-list/survey-list.spec.tsx
+++ src/application/pages/survey-list/survey-list.spec.tsx
@@ -1,14 +1,33 @@
 import { SurveyList } from '@/application/pages'
+import { type SurveyModel } from '@/domain/models'
+import { type LoadSurveyList } from '@/domain/usecases'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++
+
+    return []
+  }
+}
+
 describe('SurveyList Component', () => {
+  let loadSurveyListSpy: LoadSurveyListSpy
+
   beforeEach(() => {
-    render(<SurveyList />)
+    loadSurveyListSpy = new LoadSurveyListSpy()
+    render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
   })
 
   it('Should present 4 empty items on start', () => {
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  it('Should call LoadSurveyList', () => {
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
--- src/application/pages/survey-list/survey-list.tsx
+++ src/application/pages/survey-list/survey-list.tsx
@@ -1,9 +1,20 @@
 import { Footer, Header } from '@/application/components'
 import { SurveyItemEmpty } from '@/application/pages/survey-list/components'
-import React from 'react'
+import { type LoadSurveyList } from '@/domain/usecases'
+import React, { useEffect } from 'react'
 import Styles from './survey-list-styles.scss'
 
-const SurveyList: React.FC = () => {
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  useEffect(() => {
+    (async function () {
+      loadSurveyList.loadAll()
+    })()
+  }, [])
+
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
