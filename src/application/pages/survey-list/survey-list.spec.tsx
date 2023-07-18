e571891a152cbf88f79eff6cad346fea14e8e865
José Divino <josedivinooficial02@gmail.com>
2023-07-18 19:17:11 -0300

    feat: ensure SurveyItems render on success

--- src/application/pages/survey-list/survey-list.spec.tsx
+++ src/application/pages/survey-list/survey-list.spec.tsx
@@ -1,16 +1,18 @@
 import { SurveyList } from '@/application/pages'
 import { type SurveyModel } from '@/domain/models'
+import { mockSurveyListModel } from '@/domain/test'
 import { type LoadSurveyList } from '@/domain/usecases'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
+  surveys = mockSurveyListModel()
 
   async loadAll(): Promise<SurveyModel[]> {
     this.callsCount++
 
-    return []
+    return this.surveys
   }
 }
 
@@ -22,12 +24,22 @@ describe('SurveyList Component', () => {
     render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
   })
 
-  it('Should present 4 empty items on start', () => {
+  it('Should present 4 empty items on start', async () => {
     const surveyList = screen.getByTestId('survey-list')
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
 
-  it('Should call LoadSurveyList', () => {
+  it('Should call LoadSurveyList', async () => {
     expect(loadSurveyListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  it('Should render SurveyItems on success', async () => {
+    const surveyList = screen.getByTestId('survey-list')
+
+    await waitFor(() => surveyList)
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(3)
   })
 })
--- src/application/pages/survey-list/survey-list.tsx
+++ src/application/pages/survey-list/survey-list.tsx
@@ -1,7 +1,8 @@
 import { Footer, Header } from '@/application/components'
-import { SurveyItemEmpty } from '@/application/pages/survey-list/components'
+import { SurveyItem, SurveyItemEmpty } from '@/application/pages/survey-list/components'
+import { type SurveyModel } from '@/domain/models'
 import { type LoadSurveyList } from '@/domain/usecases'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './survey-list-styles.scss'
 
 type Props = {
@@ -9,10 +10,12 @@ type Props = {
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[]
+  })
+
   useEffect(() => {
-    (async function () {
-      loadSurveyList.loadAll()
-    })()
+    loadSurveyList.loadAll().then(surveys => { setState({ surveys }); })
   }, [])
 
   return (
@@ -21,7 +24,10 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul data-testid="survey-list">
-          <SurveyItemEmpty />
+          {state.surveys.length
+            ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
+            : <SurveyItemEmpty />
+          }
         </ul>
       </div>
       <Footer />
