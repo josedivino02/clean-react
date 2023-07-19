a53ae5f1ce422c4ae560bdfc00b5c80fd16a9b9d
José Divino <josedivinooficial02@gmail.com>
2023-07-18 21:00:31 -0300

    feat: change LoadSurveyList error layout

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -4,5 +4,5 @@ import { makeApiUrl } from '@/main/factories/http/api-url-factory';
 import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('signup'), makeAxiosHttpClient());
+  return new RemoteLoadSurveyList(makeApiUrl('surveys'), makeAxiosHttpClient());
 };
