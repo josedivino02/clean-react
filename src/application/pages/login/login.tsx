9ec68b25ef80df3c951105d5b4df5b0aede53f9e
José Divino <josedivinooficial02@gmail.com>
2023-07-24 08:03:11 -0300

    refactor: change useState to use callback

--- src/application/pages/login/login.tsx
+++ src/application/pages/login/login.tsx
@@ -40,6 +40,16 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     })
   }, [state.email, state.password])
 
+  // useEffect(() => { validate('email') }, [state.email])
+  // useEffect(() => { validate('password') }, [state.password])
+
+  // const validate = (field: string): void => {
+  //   const { email, password } = state
+  //   const formData = { email, password }
+  //   setState(old => ({ ...old, [`${field}Error`]: validation.validate(field, formData) }))
+  //   setState(old => ({ ...old, isFormInvalid: !!old.emailError || !!old.passwordError }))
+  // }
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     try {
