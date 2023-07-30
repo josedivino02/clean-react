fd6132207978c77696b1b505e500cdeeb095e78c
José Divino <josedivinooficial02@gmail.com>
2023-07-30 18:44:34 -0300

    test: ensure SurveyResult logout if request returns 403

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -7,6 +7,10 @@ export const mockUnexpectedError = (): void => {
   Http.mockServerError(path, 'GET');
 };
 
+export const mockAccessDeniedError = (): void => {
+  Http.mockForbiddenError(path, 'GET');
+};
+
 export const mockSuccess = (): void => {
   Http.mockOk(path, 'GET', 'fx:survey-result');
 };
@@ -37,5 +41,11 @@ describe('SurveyResult', () => {
     mockSuccess();
     cy.getByTestId('reload').click();
     cy.getByTestId('question').should('exist');
+  });
+
+  it('Should logout on AccessDeniedError', () => {
+    mockAccessDeniedError();
+    cy.visit('/surveys/any_id');
+    Helper.testUrl('login');
   });
 });
