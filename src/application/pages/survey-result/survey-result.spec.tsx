c5d2737cc1fb68a8acbf8761ab8e3704bd82ab1d
José Divino <josedivinooficial02@gmail.com>
2023-08-01 19:39:37 -0300

    feat: ensure SurveyResult present new result on answer click

--- src/application/pages/survey-result/survey-result.spec.tsx
+++ src/application/pages/survey-result/survey-result.spec.tsx
@@ -1,7 +1,7 @@
 import ApiContext from '@/application/contexts/api/api-context'
 import { AccessDeniedError } from '@/domain/errors'
 import { type AccountModel } from '@/domain/models'
-import { LoadSurveyResultSpy, SaveSurveyResultSpy, mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, SaveSurveyResultSpy, mockAccountModel, mockSurveyResultModel } from '@/domain/test'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { createMemoryHistory, type MemoryHistory } from 'history'
 import React from 'react'
@@ -76,10 +76,10 @@ describe('SurveyResult Component', () => {
   //   expect(images[1]).toBeFalsy()
   //   const answers = screen.queryAllByTestId('answer')
   //   expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
-  //   expect(answers[1]).toHaveAttribute(surveyResult.answers[1].answer)
+  //   expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
   //   const percents = screen.queryAllByTestId('percent')
   //   expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
-  //   expect(percents[1]).toHaveAttribute(`${surveyResult.answers[1].percent}%`)
+  //   expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
   // })
 
   // it('Should render error on UnexpectedError', async () => {
@@ -181,5 +181,34 @@ describe('SurveyResult Component', () => {
     await waitFor(() => screen.getByTestId('survey-result'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(history.location.pathname).toBe('/login')
+  })
+
+  it('Should present SurveyResult data on SaveSurveyResult success', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2023-08-01T00:00:00')
+    })
+    saveSurveyResultSpy.surveyResult = surveyResult
+    await waitFor(() => screen.getByTestId('survey-result'))
+    const answerWrap = screen.queryAllByTestId('answer-wrap')
+    fireEvent.click(answerWrap[1])
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(screen.getByTestId('day')).toHaveTextContent('01')
+    expect(screen.getByTestId('month')).toHaveTextContent('ago')
+    expect(screen.getByTestId('year')).toHaveTextContent('2023')
+    expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question)
+    expect(screen.getByTestId('answers').childElementCount).toBe(2)
+    expect(answerWrap[0]).toHaveClass('active')
+    expect(answerWrap[1]).not.toHaveClass('active')
+    const images = screen.queryAllByTestId('image')
+    expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+    expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+    const answers = screen.queryAllByTestId('answer')
+    expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+    expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+    const percents = screen.queryAllByTestId('percent')
+    expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+    expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
   })
 })
